01afcadec9d9b35b09c0cd3b212eb96d4aca5d09
Eva-Maria Stetter <eva-maria.stetter@iml.fraunhofer.de>
2025-11-10 15:20:41 +0100

    add header

--- apps/frontend/src/app/pages/bottling/details/red-compliance/red-compliance.component.ts
+++ apps/frontend/src/app/pages/bottling/details/red-compliance/red-compliance.component.ts
@@ -1,13 +1,20 @@
+/*
+ * Copyright Fraunhofer Institute for Material Flow and Logistics
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * For details on the licensing terms, see the LICENSE file.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import { FINANCIAL_SUPPORT_INFO } from 'apps/frontend/src/app/shared/constants/financial-support-info';
 import { Subject } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import { Component, input } from '@angular/core';
-import { MatTooltip } from '@angular/material/tooltip';
 import { RedComplianceDto } from '@h2-trust/api';
 
 @Component({
   selector: 'app-red-compliance',
-  imports: [CommonModule, MatTooltip],
+  imports: [CommonModule],
   templateUrl: './red-compliance.component.html',
 })
 export class RedComplianceComponent {
--- apps/frontend/src/app/shared/constants/financial-support-info.ts
+++ apps/frontend/src/app/shared/constants/financial-support-info.ts
@@ -1,2 +1,10 @@
+/*
+ * Copyright Fraunhofer Institute for Material Flow and Logistics
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * For details on the licensing terms, see the LICENSE file.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 export const FINANCIAL_SUPPORT_INFO =
   'Received financial support in the form of operating subsidies or investment subsidies';
