1eaf6645c967e79602984d7a8fb282baab1775ac
Eva-Maria Stetter <eva-maria.stetter@iml.fraunhofer.de>
2025-12-01 09:06:17 +0100

    fix format issues

--- libs/api/src/lib/dtos/production/intervall-matching-result.dto.ts
+++ libs/api/src/lib/dtos/production/intervall-matching-result.dto.ts
@@ -10,25 +10,19 @@ import { IntervallMatchingResultEntity } from '@h2-trust/amqp';
 
 export class IntervallMatchingResultDto {
   id: string;
-  createdAt: Date;
 
   powerUsed: number;
   hydrogenProduced: number;
   numberOfBatches: number;
 
-  constructor(id: string, createdAt: Date, powerUsed: number, hydrogenProduced: number, numberOfBatches: number) {
+  constructor(id: string, powerUsed: number, hydrogenProduced: number, numberOfBatches: number) {
     this.id = id;
-    (this.createdAt = createdAt), (this.powerUsed = powerUsed);
-    (this.hydrogenProduced = hydrogenProduced), (this.numberOfBatches = numberOfBatches);
+    this.powerUsed = powerUsed;
+    this.hydrogenProduced = hydrogenProduced;
+    this.numberOfBatches = numberOfBatches;
   }
 
   static fromDatabase(entity: IntervallMatchingResultEntity) {
-    return new IntervallMatchingResultDto(
-      entity.id,
-      entity.createdAt,
-      entity.powerUsed,
-      entity.hydrogenProduced,
-      entity.numberOfBatches,
-    );
+    return new IntervallMatchingResultDto(entity.id, entity.powerUsed, entity.hydrogenProduced, entity.numberOfBatches);
   }
 }
