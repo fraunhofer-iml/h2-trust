e84d395cdd9c76d9de9da7e2b4e862e16b459308
Eva-Maria Stetter <eva-maria.stetter@iml.fraunhofer.de>
2025-12-10 14:35:33 +0100

    add headings to pie chart legend

--- apps/frontend/src/app/pages/bottling/details/proof-of-sustainability/emission-pie-chart/emission-pie-chart.component.ts
+++ apps/frontend/src/app/pages/bottling/details/proof-of-sustainability/emission-pie-chart/emission-pie-chart.component.ts
@@ -9,23 +9,27 @@
 import * as echarts from 'echarts';
 import { EChartsOption, PieSeriesOption } from 'echarts';
 import { NgxEchartsDirective, provideEchartsCore } from 'ngx-echarts';
-import { CommonModule, PercentPipe } from '@angular/common';
+import { CommonModule, PercentPipe, TitleCasePipe } from '@angular/common';
 import { Component, computed, inject, input } from '@angular/core';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { EmissionForProcessStepDto } from '@h2-trust/api';
+import { EmissionForProcessStepDto, EmissionProcessStepType } from '@h2-trust/api';
 import { FormattedUnits } from '../../../../../shared/constants/formatted-units';
 
 @Component({
   selector: 'app-emission-pie-chart',
   imports: [CommonModule, NgxEchartsDirective, MatProgressSpinnerModule],
-  providers: [provideEchartsCore({ echarts }), PercentPipe],
+  providers: [provideEchartsCore({ echarts }), PercentPipe, TitleCasePipe],
 
   templateUrl: './emission-pie-chart.component.html',
 })
 export class EmissionPieChartComponent {
   percentPipe = inject(PercentPipe);
+  titleCasePipe = inject(TitleCasePipe);
+
   data = input<EmissionForProcessStepDto[]>([]);
   chartData$ = computed(() => this.toChartData(this.data()));
+
+  readonly availableEmissionProcessStepTypes: EmissionProcessStepType[] = ['APPLICATION', 'REGULATORY'] as const;
 
   private toChartData(emissionItems: EmissionForProcessStepDto[]): EChartsOption {
     const applicationItems = emissionItems.filter((i) => i.processStepType === 'APPLICATION');
@@ -38,22 +42,14 @@ export class EmissionPieChartComponent {
       const outerColors = ['#7a7aad', '#9e9ed8', '#b6b6ff', '#c2c2ff', '#d1d1ff', '#eeeeff', '#e0e0ff'];
       const innerColors = ['#004D40', '#00796B', '#009688', '#4DB6AC', '#80CBC4', '#B2DFDB', '#E0F2F1'];
 
-      const outerPie = this.createPieSeries(
-        ['50%', '80%'],
-        'outer',
-        regulatoryItems,
-        outerColors,
-        regulatoryItems,
-        applicationItems,
-      );
-      const innerPie = this.createPieSeries(
-        ['15%', '45%'],
-        'inner',
-        applicationItems,
-        innerColors,
-        regulatoryItems,
-        applicationItems,
-      );
+      const outerName: EmissionProcessStepType = 'REGULATORY';
+      const innerName: EmissionProcessStepType = 'APPLICATION';
+
+      const outerPie = this.createPieSeries(['40%', '60%'], 'outer', regulatoryItems, outerColors, emissionItems);
+      outerPie.name = outerName;
+
+      const innerPie = this.createPieSeries(['10%', '35%'], 'inner', applicationItems, innerColors, emissionItems);
+      innerPie.name = innerName;
 
       chartOption.series = [outerPie, innerPie];
     }
@@ -89,13 +85,9 @@ export class EmissionPieChartComponent {
     };
   }
 
-  private readonly tooltipFormatter = (
-    params: any,
-    dataByItems: EmissionForProcessStepDto[],
-    dataTotal: EmissionForProcessStepDto[],
-  ): string => {
+  private readonly tooltipFormatter = (params: any, dataTotal: EmissionForProcessStepDto[]): string => {
     const percent = this.percentPipe.transform((params.percent ?? 0) / 100, '1.0-1');
-    const description = [...dataByItems, ...dataTotal].find((item) => item.name === params.name)?.description;
+    const description = dataTotal.find((item) => item.name === params.name)?.description;
     return `${params.marker} ${params.name} (${description}): ${params.value} ${FormattedUnits.CO2_PER_KG_H2} (${percent})`;
   };
 
@@ -105,31 +97,49 @@ export class EmissionPieChartComponent {
   ): EChartsOption => ({
     title: { text: '' },
     tooltip: {},
-    legend: {
-      right: 30,
-      top: 'center',
-      show: true,
-      selectedMode: false,
-      orient: 'vertical',
-      textStyle: {
-        color: '#ababab',
+    legend: [
+      this.getHeading('REGULATORY'),
+      {
+        left: '60%',
+        top: '16%',
+        show: true,
+        selectedMode: false,
+        orient: 'vertical',
+        textStyle: {
+          color: '#ababab',
+        },
+        data: [...regulatoryItems.map((i) => i.name)],
+        formatter: function (name) {
+          const description = [...regulatoryItems].find((item) => item.name === name)?.description;
+          return name + ` (${description})`;
+        },
       },
-      data: [...applicationItems.map((i) => i.name), ...regulatoryItems.map((i) => i.name)],
-      formatter: function (name) {
-        const description = [...regulatoryItems, ...applicationItems].find((item) => item.name === name)?.description;
-        return name + ` (${description})`;
+      this.getHeading('APPLICATION'),
+      {
+        left: '60%',
+        top: '50%',
+        show: true,
+        selectedMode: false,
+        orient: 'vertical',
+        textStyle: {
+          color: '#ababab',
+        },
+        data: [...applicationItems.map((i) => i.name)],
+        formatter: function (name) {
+          const description = [...applicationItems].find((item) => item.name === name)?.description;
+          return name + ` (${description})`;
+        },
       },
-    },
+    ],
     series: [],
   });
 
   private createPieSeries(
     radius: [string, string],
     position: 'inner' | 'outer',
-    data: { amount: number; name: string }[],
+    data: EmissionForProcessStepDto[],
     colors: string[],
-    dataByItems: EmissionForProcessStepDto[],
-    dataTotal: EmissionForProcessStepDto[],
+    totalItems: EmissionForProcessStepDto[],
   ): PieSeriesOption {
     const pieSeries = this.getDefaultPieSeries(radius);
     pieSeries.label = {
@@ -141,7 +151,25 @@ export class EmissionPieChartComponent {
       name: item.name,
       itemStyle: { color: colors[index] },
     }));
-    pieSeries.tooltip = { formatter: (params) => this.tooltipFormatter(params, dataByItems, dataTotal) };
+    pieSeries.tooltip = { formatter: (params) => this.tooltipFormatter(params, totalItems) };
     return pieSeries;
+  }
+
+  private getHeading(type: EmissionProcessStepType) {
+    return {
+      data: [type as string],
+      top: type === 'APPLICATION' ? '10%' : '44%',
+      left: '56%',
+      selectedMode: false,
+      icon: 'none',
+      textStyle: {
+        color: '#787a78',
+        fontSize: 12,
+        fontWeight: 'bolder',
+      },
+      formatter: (params) => {
+        return this.titleCasePipe.transform(params).trim();
+      },
+    } as echarts.LegendComponentOption;
   }
 }
