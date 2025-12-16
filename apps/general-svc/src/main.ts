619bd1833b71efea9b26403bbf6423fa6fefa5f2
Michael Pichura <mipicdev@mailbox.org>
2025-12-16 09:07:45 +0100

    remove implicit conversion from ValidationPipe in main.ts

--- apps/general-svc/src/main.ts
+++ apps/general-svc/src/main.ts
@@ -36,10 +36,7 @@ async function bootstrap() {
     new ValidationPipe({
       transform: true,
       whitelist: true,
-      forbidNonWhitelisted: true,
-      transformOptions: {
-        enableImplicitConversion: true,
-      },
+      forbidNonWhitelisted: true
     }),
   );
   app.useGlobalInterceptors(new PrismaErrorsInterceptor());
