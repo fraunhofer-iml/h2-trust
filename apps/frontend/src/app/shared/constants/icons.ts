61fb84d17436a10791a3e7ffb38b421699a1ea75
Eva-Maria Stetter <eva-maria.stetter@iml.fraunhofer.de>
2025-12-18 10:14:59 +0100

    add const for icons

---
+++ apps/frontend/src/app/shared/constants/icons.ts
@@ -0,0 +1,15 @@
+export const ICONS = {
+  UNITS: {
+    POWER_PRODUCTION: 'electric_bolt',
+    HYDROGEN_PRODUCTION: 'water_ec',
+    HYDROGEN_STORAGE: 'propane',
+  },
+  PROCESS_STEPS: {
+    WATER_SUPPLY: 'water_drop',
+    POWER_SUPPLY: 'electric_bolt',
+    TRANSPORTATION: 'road',
+    HYDROGEN_BOTTLING: 'gas_meter',
+    HYDROGEN_STORAGE: 'propane',
+    HYDROGEN_PRODUCTION: 'water_ec',
+  },
+};
