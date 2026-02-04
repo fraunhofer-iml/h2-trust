244c42cc45694390c828b660c3ad551436eadc58
lgrieger <lukas.grieger@iml.fraunhofer.de>
2026-02-04 15:43:26 +0100

    remove company mock element

--- libs/database/src/lib/types/mocks/company-db-type.mock.ts
+++ libs/database/src/lib/types/mocks/company-db-type.mock.ts
@@ -6,51 +6,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { CompanyDbShallowType, CompanyDbSurfaceType, CompanyDeepDbType } from '..';
+import { CompanyDbShallowType, CompanyDeepDbType } from '..';
 import { AddressSeed, CompanySeed } from '../../../seed';
-
-export const CompanySurfaceDbTypeMock: readonly CompanyDbSurfaceType[] = Object.freeze([
-  {
-    ...CompanySeed[0],
-    address: AddressSeed[0],
-    hydrogenApprovals: [],
-    powerApprovals: [],
-    unitOwners: [],
-    unitOperators: [],
-    users: [],
-    batches: [],
-  },
-  {
-    ...CompanySeed[1],
-    address: AddressSeed[1],
-    hydrogenApprovals: [],
-    powerApprovals: [],
-    unitOwners: [],
-    unitOperators: [],
-    users: [],
-    batches: [],
-  },
-  {
-    ...CompanySeed[2],
-    address: AddressSeed[2],
-    hydrogenApprovals: [],
-    powerApprovals: [],
-    unitOwners: [],
-    unitOperators: [],
-    users: [],
-    batches: [],
-  },
-  {
-    ...CompanySeed[3],
-    address: AddressSeed[3],
-    hydrogenApprovals: [],
-    powerApprovals: [],
-    unitOwners: [],
-    unitOperators: [],
-    users: [],
-    batches: [],
-  },
-]);
 
 export const CompanyShallowDbTypeMock = <CompanyDbShallowType[]>[
   {
