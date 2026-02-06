fd42dcf6b0e0476a8088e2dfd472dd0db44094e3
Eva-Maria Stetter <eva-maria.stetter@iml.fraunhofer.de>
2026-02-06 12:11:13 +0100

    format and lint

--- apps/bff/src/app/file-download/file-download.controller.ts
+++ apps/bff/src/app/file-download/file-download.controller.ts
@@ -1,3 +1,11 @@
+/*
+ * Copyright Fraunhofer Institute for Material Flow and Logistics
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * For details on the licensing terms, see the LICENSE file.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import type { Response } from 'express';
 import { Body, Controller, Post, Res } from '@nestjs/common';
 import { DownloadFilesDto } from '@h2-trust/api';
--- apps/frontend/src/app/pages/production/uploaded-production-files/download-button/download-button.component.ts
+++ apps/frontend/src/app/pages/production/uploaded-production-files/download-button/download-button.component.ts
@@ -1,3 +1,11 @@
+/*
+ * Copyright Fraunhofer Institute for Material Flow and Logistics
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * For details on the licensing terms, see the LICENSE file.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import { ProductionService } from 'apps/frontend/src/app/shared/services/production/production.service';
 import { toast } from 'ngx-sonner';
 import { SelectionModel } from '@angular/cdk/collections';
