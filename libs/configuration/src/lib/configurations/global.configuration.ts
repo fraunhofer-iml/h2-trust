57644de94e44e42536fbfecad4b2b9f276f01ee3
Michael Pichura <mipicdev@mailbox.org>
2026-02-09 11:28:26 +0100

    improve blockchain configuration options

--- libs/configuration/src/lib/configurations/global.configuration.ts
+++ libs/configuration/src/lib/configurations/global.configuration.ts
@@ -21,16 +21,16 @@ export interface MinioConfiguration {
 }
 
 export interface BlockchainConfiguration {
+  enabled: boolean;
   rpcUrl: string;
   privateKey: string;
-  smartContractAddress: string;
   artifactPath: string;
+  smartContractAddress: string;
 }
 
 export interface GlobalConfiguration {
   logLevel: LogLevel[];
   amqpUri: string;
-  blockchainEnabled: boolean;
   blockchain: BlockchainConfiguration;
   minio: MinioConfiguration;
 }
@@ -38,12 +38,12 @@ export interface GlobalConfiguration {
 export default registerAs(GLOBAL_CONFIGURATION_IDENTIFIER, () => ({
   logLevel: (process.env['LOG_LEVEL'] || 'fatal,error,warn,log,debug').split(','),
   amqpUri: process.env['AMQP_URI'] || 'amqp://localhost:5672',
-  blockchainEnabled: process.env['BLOCKCHAIN_ENABLED'] === 'true',
   blockchain: {
-    rpcUrl: process.env['BC_RPC_URL'] || '',
-    privateKey: process.env['BC_PRIVATE_KEY'] || '',
-    smartContractAddress: process.env['BC_SMART_CONTRACT'] || '',
-    artifactPath: process.env['BC_ARTIFACT_PATH'] || '',
+    enabled: process.env['BLOCKCHAIN_ENABLED'] === 'true',
+    rpcUrl: process.env['BLOCKCHAIN_RPC_URL'] || '',
+    privateKey: process.env['BLOCKCHAIN_PRIVATE_KEY'] || '',
+    artifactPath: process.env['BLOCKCHAIN_ARTIFACT_PATH'] || '',
+    smartContractAddress: process.env['BLOCKCHAIN_SMART_CONTRACT'] || '',
   },
   minio: {
     endPoint: process.env['MINIO_ENDPOINT'] || 'localhost',
