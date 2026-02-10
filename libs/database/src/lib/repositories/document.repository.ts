7f0992222f5e9fc9a4274e351224880cd79e482e
Michael Pichura <mipicdev@mailbox.org>
2026-02-10 09:27:43 +0100

    introduce methods to create and update documents by file name and transaction hash

--- libs/database/src/lib/repositories/document.repository.ts
+++ libs/database/src/lib/repositories/document.repository.ts
@@ -13,11 +13,30 @@ import { PrismaService } from '../prisma.service';
 
 @Injectable()
 export class DocumentRepository {
-  constructor(private readonly prismaService: PrismaService) {}
+  constructor(private readonly prismaService: PrismaService) { }
 
   async addDocumentToProcessStep(document: DocumentEntity, processStepId: string): Promise<DocumentEntity> {
     return this.prismaService.document
       .create({ data: buildDocumentCreateInput(document, processStepId) })
       .then(DocumentEntity.fromDatabase);
+  }
+
+  async createDocumentsWithFileName(fileNames: string[]): Promise<DocumentEntity[]> {
+    const documents = await this.prismaService.document
+      .createManyAndReturn({
+        data: fileNames.map((fileName) => ({ fileName }))
+      });
+
+    return documents.map(DocumentEntity.fromDatabase);
+  }
+
+  async updateDocumentsWithTransactionHash(documentIds: string[], transactionHash: string): Promise<DocumentEntity[]> {
+    const documents = await this.prismaService.document
+      .updateManyAndReturn({
+        where: { id: { in: documentIds } },
+        data: { transactionHash },
+      });
+
+    return documents.map(DocumentEntity.fromDatabase);
   }
 }
