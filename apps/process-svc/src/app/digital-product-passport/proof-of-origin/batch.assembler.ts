fb493edff050ff532e3a6042d0ba09ca5d8dd335
Lukas Wegler <lukas.wegler@iml.fraunhofer.de>
2026-03-03 12:20:17 +0100

    changes batchIds in Batch assembler to process-step-id

--- apps/process-svc/src/app/digital-product-passport/proof-of-origin/batch.assembler.ts
+++ apps/process-svc/src/app/digital-product-passport/proof-of-origin/batch.assembler.ts
@@ -23,7 +23,7 @@ export class BatchAssembler {
     emission?: ProofOfOriginEmissionEntity,
   ): ProofOfOriginPowerBatchEntity {
     return {
-      id: powerProduction.batch.id,
+      id: powerProduction.id,
       emission,
       createdAt: powerProduction.startedAt,
       amount: powerProduction.batch.amount,
@@ -40,7 +40,7 @@ export class BatchAssembler {
     emission?: ProofOfOriginEmissionEntity,
   ): ProofOfOriginWaterBatchEntity {
     return {
-      id: waterConsumption.batch.id,
+      id: waterConsumption.id,
       emission,
       createdAt: waterConsumption.startedAt,
       amount: waterConsumption.batch.amount,
@@ -55,7 +55,7 @@ export class BatchAssembler {
     emission?: ProofOfOriginEmissionEntity,
   ): ProofOfOriginHydrogenBatchEntity {
     return {
-      id: hydrogenStorage.batch.id,
+      id: hydrogenStorage.id,
       emission,
       createdAt: hydrogenStorage.startedAt,
       amount: hydrogenStorage.batch.amount,
@@ -82,7 +82,7 @@ export class BatchAssembler {
     emission?: ProofOfOriginEmissionEntity,
   ): ProofOfOriginHydrogenBatchEntity {
     return {
-      id: hydrogenBottling.batch.id,
+      id: hydrogenBottling.id,
       emission,
       createdAt: hydrogenBottling.startedAt,
       amount: hydrogenBottling.batch.amount,
@@ -101,7 +101,7 @@ export class BatchAssembler {
     emission?: ProofOfOriginEmissionEntity,
   ): ProofOfOriginHydrogenBatchEntity {
     return {
-      id: hydrogenTransportation.batch.id,
+      id: hydrogenTransportation.id,
       emission,
       createdAt: hydrogenTransportation.startedAt,
       amount: hydrogenTransportation.batch.amount,
