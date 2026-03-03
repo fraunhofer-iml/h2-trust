077daa43b7eb5feae3eea113e51cfa77598f70bc
Michael Pichura <mipicdev@mailbox.org>
2026-03-03 15:44:40 +0100

    use the same password for all services

--- libs/configuration/src/lib/configurations/global.configuration.ts
+++ libs/configuration/src/lib/configurations/global.configuration.ts
@@ -60,7 +60,7 @@ export default registerAs(GLOBAL_CONFIGURATION_IDENTIFIER, () => ({
     endPoint: process.env['MINIO_ENDPOINT'] || 'localhost',
     port: parseInt(process.env['MINIO_PORT'] || '9000'),
     accessKey: process.env['MINIO_ACCESS_KEY'] || 'admin',
-    secretKey: process.env['MINIO_SECRET_KEY'] || 'blockchain',
+    secretKey: process.env['MINIO_SECRET_KEY'] || 'admin12345',
     bucketName: process.env['MINIO_BUCKET_NAME'] || 'h2-trust',
   },
   blockchain: {
