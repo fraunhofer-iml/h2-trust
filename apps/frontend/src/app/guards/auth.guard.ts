619784c61cc859a7c91e36016efd9406609d5ef4
Eva-Maria Stetter <eva-maria.stetter@iml.fraunhofer.de>
2026-03-05 15:57:02 +0100

    enhance(fe): migrate keycloak (#117)
    - migrate Keycloak angular to v19+
    
    closes DUHGW-384

--- apps/frontend/src/app/guards/auth.guard.ts
+++ apps/frontend/src/app/guards/auth.guard.ts
@@ -6,13 +6,25 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { KeycloakService } from 'keycloak-angular';
+import { AuthGuardData, createAuthGuard } from 'keycloak-angular';
+import Keycloak from 'keycloak-js';
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivateFn, RouterStateSnapshot, UrlTree } from '@angular/router';
 
-export const AUTH_GUARD: CanActivateFn = async () => {
-  const keycloakService: KeycloakService = inject(KeycloakService);
-  const isLoggedIn = keycloakService.isLoggedIn();
-  if (!isLoggedIn) keycloakService.login();
-  return isLoggedIn;
+const isAccessAllowed = async (
+  _route: ActivatedRouteSnapshot,
+  _: RouterStateSnapshot,
+  authData: AuthGuardData,
+): Promise<boolean | UrlTree> => {
+  const keycloak = inject(Keycloak);
+
+  const { authenticated } = authData;
+
+  if (!authenticated) {
+    keycloak.login();
+  }
+
+  return authenticated;
 };
+
+export const canActivateAuth = createAuthGuard<CanActivateFn>(isAccessAllowed);
