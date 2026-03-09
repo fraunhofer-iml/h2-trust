f3a76fa54ae9de67a6fdeb74856ce8ec9c481ed7
lgrieger <lukas.grieger@iml.fraunhofer.de>
2026-03-09 15:40:39 +0100

    fix reference error

--- apps/process-svc/src/app/digital-product-passport/digital-product-passport.service.ts
+++ apps/process-svc/src/app/digital-product-passport/digital-product-passport.service.ts
@@ -150,10 +150,10 @@ export class DigitalProductPassportService {
 
   private static getPowerProductionClass(provenance: ProvenanceEntity): PowerProductionClass {
     const nonRenewablePowerProductionExits: boolean = provenance.powerProductions
-      .map((powerProductions) => powerProductions.batch?.qualityDetails?.powerClass)
+      .map((powerProductions) => powerProductions.batch?.qualityDetails?.powerProductionClass)
       .some((powerClassification) => powerClassification == PowerProductionClass.NOT_RENEWABLE_GRID);
     const renewablePowerProductionExits: boolean = provenance.powerProductions
-      .map((powerProductions) => powerProductions.batch?.qualityDetails?.powerClass)
+      .map((powerProductions) => powerProductions.batch?.qualityDetails?.powerProductionClass)
       .some((powerClassification) => powerClassification == PowerProductionClass.RENEWABLE);
 
     if (nonRenewablePowerProductionExits) {
