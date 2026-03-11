fe59b82a8160003e2efcd4a6436fb4e2db1ed6e6
lgrieger <lukas.grieger@iml.fraunhofer.de>
2026-03-11 09:53:08 +0100

    modify batch quality seed

--- libs/database/src/seed/data/batch/batch-details.seed.ts
+++ libs/database/src/seed/data/batch/batch-details.seed.ts
@@ -10,6 +10,7 @@ import { BatchDetails } from '@prisma/client';
 import { HydrogenBottlingBatchSeed } from './hydrogen-bottling-batch.seed';
 import { HydrogenProductionBatchSeed } from './hydrogen-production-batch.seed';
 import { HydrogenTransportationBatchSeed } from './hydrogen-transportation-batch.seed';
+import { PowerProductionBatchSeed } from './power-production-batch.seed';
 import { QualityDetailsSeed } from './quality-details.seed';
 
 export const BatchDetailsSeed: readonly BatchDetails[] = Object.freeze([
@@ -76,5 +77,46 @@ export const BatchDetailsSeed: readonly BatchDetails[] = Object.freeze([
   {
     id: QualityDetailsSeed[15].id,
     batchId: HydrogenTransportationBatchSeed[2].id,
+  },
+
+  {
+    id: QualityDetailsSeed[16].id,
+    batchId: PowerProductionBatchSeed[0].id,
+  },
+  {
+    id: QualityDetailsSeed[17].id,
+    batchId: PowerProductionBatchSeed[1].id,
+  },
+  {
+    id: QualityDetailsSeed[18].id,
+    batchId: PowerProductionBatchSeed[2].id,
+  },
+  {
+    id: QualityDetailsSeed[19].id,
+    batchId: PowerProductionBatchSeed[3].id,
+  },
+  {
+    id: QualityDetailsSeed[20].id,
+    batchId: PowerProductionBatchSeed[4].id,
+  },
+  {
+    id: QualityDetailsSeed[21].id,
+    batchId: PowerProductionBatchSeed[5].id,
+  },
+  {
+    id: QualityDetailsSeed[22].id,
+    batchId: PowerProductionBatchSeed[6].id,
+  },
+  {
+    id: QualityDetailsSeed[23].id,
+    batchId: PowerProductionBatchSeed[7].id,
+  },
+  {
+    id: QualityDetailsSeed[24].id,
+    batchId: PowerProductionBatchSeed[8].id,
+  },
+  {
+    id: QualityDetailsSeed[25].id,
+    batchId: PowerProductionBatchSeed[9].id,
   },
 ]);
