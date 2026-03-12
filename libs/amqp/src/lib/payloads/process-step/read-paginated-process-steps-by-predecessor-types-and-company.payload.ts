c56b73cc90a9363ad87df831a6f9a4ef97d61870
Lukas Wegler <lukas.wegler@iml.fraunhofer.de>
2026-03-12 13:38:02 +0100

    adds header

--- libs/amqp/src/lib/payloads/process-step/read-paginated-process-steps-by-predecessor-types-and-company.payload.ts
+++ libs/amqp/src/lib/payloads/process-step/read-paginated-process-steps-by-predecessor-types-and-company.payload.ts
@@ -1,3 +1,11 @@
+/*
+ * Copyright Fraunhofer Institute for Material Flow and Logistics
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * For details on the licensing terms, see the LICENSE file.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import { IsNotEmpty } from 'class-validator';
 import { ProductionDataFilter } from './production-data-filter';
 import { ReadProcessStepsByPredecessorTypesAndOwnerPayload } from './read-process-steps-by-predecessor-types-and-company.payload';
