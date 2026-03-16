afc8b85726ad4a2d534e532d9e266c8c05af1268
Michael Pichura <mipicdev@mailbox.org>
2026-03-16 12:43:18 +0100

    update ProofStorage contract to use hash instead of uuid

--- libs/blockchain/smart-contract/contracts/ProofStorage.sol
+++ libs/blockchain/smart-contract/contracts/ProofStorage.sol
@@ -2,56 +2,39 @@
 pragma solidity ^0.8.28;
 
 contract ProofStorage {
-  uint256 public constant UUID_LENGTH = 36;
-
   address public immutable owner;
-
-  struct ProofEntry {
-    string uuid;
-    string hash;
-    string cid;
-  }
 
   struct Proof {
     string hash;
     string cid;
   }
 
-  mapping(string uuid => Proof proof) private uuidToProof;
+  mapping(string hash => string cid) private hashToCid;
 
-  event ProofStored(address indexed sender, string uuid, string hash, string cid);
+  event ProofStored(address indexed sender, string hash, string cid);
 
   error NotOwner();
-  error UuidInvalidLength(string uuid);
-  error UuidAlreadyExists(string uuid);
-  error UuidNotFound(string uuid);
   error HashEmpty();
+  error HashAlreadyExists(string hash);
+  error HashNotFound(string hash);
   error CidEmpty();
 
   constructor() {
     owner = msg.sender;
   }
 
-  function storeProofs(ProofEntry[] calldata proofEntries) external {
+  function storeProofs(Proof[] calldata proofs) external {
     if (msg.sender != owner) {
       revert NotOwner();
     }
 
-    for (uint256 i = 0; i < proofEntries.length; i++) {
-      ProofEntry calldata p = proofEntries[i];
-      storeProof(p.uuid, p.hash, p.cid);
+    for (uint256 i = 0; i < proofs.length; i++) {
+      Proof calldata p = proofs[i];
+      storeProof(p.hash, p.cid);
     }
   }
 
-  function storeProof(string memory uuid, string memory hash, string memory cid) private {
-    if (bytes(uuid).length != UUID_LENGTH) {
-      revert UuidInvalidLength(uuid);
-    }
-
-    if (bytes(uuidToProof[uuid].hash).length != 0) {
-      revert UuidAlreadyExists(uuid);
-    }
-
+  function storeProof(string memory hash, string memory cid) private {
     if (bytes(hash).length == 0) {
       revert HashEmpty();
     }
@@ -60,18 +43,22 @@ contract ProofStorage {
       revert CidEmpty();
     }
 
-    uuidToProof[uuid] = Proof(hash, cid);
-
-    emit ProofStored(msg.sender, uuid, hash, cid);
-  }
-
-  function getProofByUuid(string memory uuid) external view returns (Proof memory) {
-    Proof memory proof = uuidToProof[uuid];
-
-    if (bytes(proof.hash).length == 0) {
-      revert UuidNotFound(uuid);
+    if (bytes(hashToCid[hash]).length != 0) {
+      revert HashAlreadyExists(hash);
     }
 
-    return proof;
+    hashToCid[hash] = cid;
+
+    emit ProofStored(msg.sender, hash, cid);
+  }
+
+  function getCidByHash(string memory hash) external view returns (string memory) {
+    string memory cid = hashToCid[hash];
+
+    if (bytes(cid).length == 0) {
+      revert HashNotFound(hash);
+    }
+
+    return cid;
   }
 }
