bdaac67a73e8b06936c19f9004c0fb9eb85ea5ef
Michael Pichura <mipicdev@mailbox.org>
2026-03-26 14:36:40 +0100

    chore: update BASE_URL in production environment (#179)
    Updates the BASE_URL in the frontend environment file, which is used as
    a nx configuration.

--- apps/frontend/src/environments/environment.production.ts
+++ apps/frontend/src/environments/environment.production.ts
@@ -6,7 +6,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-export const BASE_URL = 'https://h2-trust-bff.apps.blockchain-europe.iml.fraunhofer.de';
+export const BASE_URL = 'https://h2-trust-bff-production.public.apps.blockchain-europe.iml.fraunhofer.de';
 
 export const environment = {
   production: false,
