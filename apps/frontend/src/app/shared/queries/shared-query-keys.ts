e9bef0c651a70503c1498420d779b84964efdef9
Eva-Maria Stetter <eva-maria.stetter@iml.fraunhofer.de>
2026-03-26 15:06:00 +0100

    format

--- apps/frontend/src/app/shared/queries/shared-query-keys.ts
+++ apps/frontend/src/app/shared/queries/shared-query-keys.ts
@@ -1,3 +1,11 @@
+/*
+ * Copyright Fraunhofer Institute for Material Flow and Logistics
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * For details on the licensing terms, see the LICENSE file.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 export const QUERY_KEYS = {
   HYDROGEN_PRODUCTION_UNITS: ['hydrogen-production-units'],
 };
