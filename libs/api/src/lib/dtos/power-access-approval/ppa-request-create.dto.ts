44be59fcfed897201ffdad7abe0b51d7f5ba855a
Eva-Maria Stetter <eva-maria.stetter@iml.fraunhofer.de>
2026-04-02 13:46:11 +0200

    lint and format

--- libs/api/src/lib/dtos/power-access-approval/ppa-request-create.dto.ts
+++ libs/api/src/lib/dtos/power-access-approval/ppa-request-create.dto.ts
@@ -1,3 +1,11 @@
+/*
+ * Copyright Fraunhofer Institute for Material Flow and Logistics
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * For details on the licensing terms, see the LICENSE file.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import { Type } from 'class-transformer';
 import { IsDate, IsEnum, IsNotEmpty, IsString } from 'class-validator';
 import { PowerProductionType } from '@h2-trust/domain';
