5af27d26bbe1358f477f5948b302b54578b6b38e
Michael Pichura <mipicdev@mailbox.org>
2026-04-17 09:37:25 +0200

    reorder dangerfile.ts

--- dangerfile.ts
+++ dangerfile.ts
@@ -11,9 +11,12 @@ import { danger, fail, message } from 'danger';
 const pr = danger.github.pr;
 const isDependabot = pr.user.login === 'dependabot[bot]';
 
-const CONVENTIONAL_TYPES = ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test'];
+if (pr.draft) {
+  fail('Pull request is marked as Draft. Please remove this before requesting review.');
+}
 
 if (!isDependabot) {
+  const CONVENTIONAL_TYPES = ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test'];
   const match = pr.title.match(/^(\w+)(\(.+\))?!?: /);
   const conventionalType = match?.[1];
 
@@ -29,26 +32,18 @@ if (!isDependabot) {
       labels: [conventionalType],
     });
   }
+
+  if (!pr.assignees || pr.assignees.length === 0) {
+    fail('Pull request needs an assignee.');
+  }
+
+  if (!pr.body || pr.body.trim().length < 10) {
+    fail('Pull request needs a meaningful description.');
+  }
 }
 
-// Ensure package-lock.json is updated when package.json is changed
 if (danger.git.modified_files.includes('package.json') && !danger.git.modified_files.includes('package-lock.json')) {
   message(
     'Changes were made to package.json, but not to package-lock.json. Please run `npm install` and commit the lockfile.',
   );
-}
-
-// Prevent draft PR from being reviewed
-if (pr.draft) {
-  fail('Pull request is marked as Draft. Please remove this before requesting review.');
-}
-
-// Enforce assignee
-if (!isDependabot && (!pr.assignees || pr.assignees.length === 0)) {
-  fail('Pull request needs an assignee.');
-}
-
-// Enforce description
-if (!isDependabot && (!pr.body || pr.body.trim().length < 10)) {
-  fail('Pull request needs a meaningful description.');
 }
