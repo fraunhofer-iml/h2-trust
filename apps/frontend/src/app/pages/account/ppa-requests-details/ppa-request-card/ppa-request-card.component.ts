065077ba6f31eefe69984f893d788ce411579b98
Eva-Maria Stetter <eva-maria.stetter@iml.fraunhofer.de>
2026-04-20 15:19:48 +0200

    remove unused import

--- apps/frontend/src/app/pages/account/ppa-requests-details/ppa-request-card/ppa-request-card.component.ts
+++ apps/frontend/src/app/pages/account/ppa-requests-details/ppa-request-card/ppa-request-card.component.ts
@@ -17,7 +17,6 @@ import { MatDivider } from '@angular/material/divider';
 import { PpaRequestDto } from '@h2-trust/api';
 import { PowerAccessApprovalStatus, PpaRequestRole } from '@h2-trust/domain';
 import { PrettyEnumPipe } from '../../../../shared/pipes/format-enum.pipe';
-import { ConfirmationResult } from '../../dialog-data';
 import { RequestConfirmationDialogComponent } from '../ppa-confirmation/request-confirmation-dialog.component';
 
 @Component({
