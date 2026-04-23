75036cadc4f14a38fd6aaa78aef7ce51c61becec
Eva-Maria Stetter <eva-maria.stetter@iml.fraunhofer.de>
2026-04-23 13:47:50 +0200

    fix: add license comments to enum label resolvers, enum label key type, and pretty string pipe files

--- apps/frontend/src/app/shared/model/enum-label-key.type.ts
+++ apps/frontend/src/app/shared/model/enum-label-key.type.ts
@@ -1,3 +1,11 @@
+/*
+ * Copyright Fraunhofer Institute for Material Flow and Logistics
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * For details on the licensing terms, see the LICENSE file.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import { ENUM_LABEL_RESOLVERS } from '../constants/eum-label-resolvers';
 
 export type EnumLabelKey = keyof typeof ENUM_LABEL_RESOLVERS;
