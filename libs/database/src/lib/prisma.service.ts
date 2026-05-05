0645f266ccb68be3fd7d6c8a9ffa8be46f17f446
Michael Pichura <mipicdev@mailbox.org>
2026-05-05 07:52:47 +0200

    remove unnecessary async/await in onModuleInit method

--- libs/database/src/lib/prisma.service.ts
+++ libs/database/src/lib/prisma.service.ts
@@ -11,8 +11,8 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  async onModuleInit() {
-    await this.$connect();
+  onModuleInit() {
+    return this.$connect();
   }
 
   enableShutdownHooks(app: INestApplication) {
