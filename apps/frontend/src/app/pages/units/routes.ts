96a010b77bed29b64686ac7aa72a79eedd66d4b6
Eva-Maria Stetter <eva-maria.stetter@iml.fraunhofer.de>
2026-05-20 16:18:28 +0200

    feat: refactor unit update components and consolidate routing for unit details

--- apps/frontend/src/app/pages/units/routes.ts
+++ apps/frontend/src/app/pages/units/routes.ts
@@ -10,20 +10,14 @@ import { Route } from '@angular/router';
 import { CreateUnitComponent } from './create/create-unit.component';
 import { UnitDetailsPageComponent } from './details/unit-details-page.component';
 import { HydrogenAssetsComponent } from './overview/hydrogen-assets.component';
-import { HydrogenProductionUnitUpdateComponent } from './update/hydrogen-production-unit-update.component';
-import { HydrogenStorageUnitUpdateComponent } from './update/hydrogen-storage-unit-update.component';
-import { PowerProductionUnitUpdateComponent } from './update/power-production-unit-update.component';
+import { UnitUpdatePageComponent } from './update/unit-update-page.component';
 
 export const HYDROGEN_ASSETS_ROUTES: Route[] = [
   {
     path: '',
     component: HydrogenAssetsComponent,
   },
-  { path: 'power-production/:id', component: UnitDetailsPageComponent },
-  { path: 'hydrogen-storage/:id', component: UnitDetailsPageComponent },
-  { path: 'hydrogen-production/:id', component: UnitDetailsPageComponent },
+  { path: ':id', component: UnitDetailsPageComponent },
   { path: 'create', component: CreateUnitComponent },
-  { path: 'power-production/:id/edit', component: PowerProductionUnitUpdateComponent },
-  { path: 'hydrogen-production/:id/edit', component: HydrogenProductionUnitUpdateComponent },
-  { path: 'hydrogen-storage/:id/edit', component: HydrogenStorageUnitUpdateComponent },
+  { path: ':id/edit', component: UnitUpdatePageComponent },
 ];
